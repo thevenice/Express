75c3cedf967de910b14403be23cfd7a852f0fac4
thevenice <pp886655@gmail.com>
2019-03-05 16:48:47 +0530

    Delete/CRUD/API/done/

--- routes/api/members.js
+++ routes/api/members.js
@@ -85,4 +85,20 @@ router.put("/:id", (req, res) => {
       .json({ msg: `ERROR UNBLE TO FIND ID NO. : ${req.params.id}` });
   }
 });
+
+//trying to make delete function
+router.delete("/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json({
+      msg: `Member with ID : ${req.params.id} is removed.`,
+      members: members.filter(member => member.id !== parseInt(req.params.id))
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No Member with Following ID: ${req.params.id}` });
+  }
+  console.log(req.params.id);
+});
 module.exports = router;
